12b5c30265f1babd5abf05bf48726d33697f8189
tomt522 <rifatjamil613@gmail.com>
2025-03-18 16:57:58 +0600

    Update help2.js

--- scripts/cmds/help2.js
+++ scripts/cmds/help2.js
@@ -63,7 +63,7 @@ module.exports = {
 			const totalCommands = commands.size;
 			msg += `\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆, 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗵𝗮𝘀 ${totalCommands} 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝘁𝗵𝗮𝘁 𝗰𝗮𝗻 𝗯𝗲 𝘂𝘀𝗲𝗱\n`;
 			msg += `𝗧𝘆𝗽𝗲 ${prefix} 𝗵𝗲𝗹𝗽 𝗰𝗺𝗱𝗡𝗮𝗺𝗲 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 𝘁𝗵𝗮𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱\n\n`;
-			msg += `♥️ | BAYJID Bot V2`; // its not decoy so change it if you want 
+			msg += `♥️ | Yₒᵤᵣ𝐓𝐎𝐌`; // its not decoy so change it if you want 
 
 			const helpListImages = [
 				'https://i.imgur.com/xyDcrW3.jpeg',
